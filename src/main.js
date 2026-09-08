d5db468ef57e98a7b65394746ac69d9c498becd7
Svyatick-RaR <petrovv.svatoslav@gmail.com>
2026-09-08 14:35:06 +0300

    попытка пят

--- src/main.js
+++ src/main.js
@@ -146,10 +146,10 @@ function analyzeSalesData(data, options) {
   return sellerStats.map((seller) => ({
     seller_id: seller.id,
     name: seller.name,
-    revenue: Number.revenue.toFixed(2),
-    profit: Number.profit.toFixed(2),
+    revenue: Number(seller.revenue.toFixed(2)),
+    profit: Number(seller.profit.toFixed(2)),
     sales_count: seller.sales_count,
     top_products: seller.top_products,
-    bonus: Number.bonus.toFixed(2)
+    bonus: Number(seller.bonus.toFixed(2))
   }));
 }
